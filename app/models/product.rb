4410551abb4b5cd56e49cb62c01a961802f7b060
Felipe Gentil <cdigentil@gmail.com>
2020-11-19 19:32:56 -0300

    add products controller/view with index

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :restaurant
+
+  validates :restaurant_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products, only: [:index]
   resources :restaurants
   devise_for :users
   root to: 'home#index'
