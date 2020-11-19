bbfc666bf637e5353854aff27d77f2070c190782
Felipe Gentil <cdigentil@gmail.com>
2020-11-19 19:52:07 -0300

    add form to create product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,26 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { render :index, notice: 'Product created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :value, :restaurant_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :products, only: [:index]
+  resources :products, only: [:index, :new, :create]
   resources :restaurants
   devise_for :users
   root to: 'home#index'
